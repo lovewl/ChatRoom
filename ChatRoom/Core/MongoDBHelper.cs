1f9ff6d911d027ccf1c3fab51c55913077bb4ad9
wuliang <wlamn2@163.com>
2018-05-25 15:20:33 +0800

    提交项目文件

---
+++ ChatRoom/Core/MongoDBHelper.cs
@@ -0,0 +1,18 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatRoom
+{
+    public class MongoDBHelper
+    {
+        private MongoClient priClient;
+        private IMongoDatabase priDatabase;
+        public MongoDBHelper() { if (priClient == null) priClient = new MongoClient("mongodb://localhost:27017"); }
+        public IMongoDatabase GetDatabase(string dbName) => priClient.GetDatabase(dbName);
+        public IMongoCollection<BsonDocument> GetCollection(string dbName, string cName) => GetDatabase(dbName).GetCollection<BsonDocument>(cName);
+    }
+}
